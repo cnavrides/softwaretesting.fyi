1c3364f5676e1e5b4c7797d21a7878f60e0e716e
Chris Navrides <chris@test.ai>
2022-07-30 12:11:30 -0700

    fix edit url link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -39,7 +39,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/cnavrides/softwaretesting.fyi/blob/main/README.md',
+            'https://github.com/cnavrides/softwaretesting.fyi/tree/main',
         },
         // blog: {
         //   showReadingTime: true,
